68cb949528a9b9d86aff997f31929933bb4172d4
mamu9533 <mamu9533@colorado.edu>
2026-04-25 23:05:02 -0600

    added a pyrcel shortcut

--- cloud_parcel/__init__.py
+++ cloud_parcel/__init__.py
@@ -1,5 +1,6 @@
 from cloud_parcel.pyrcel_runner import Cloud_Parcel
 from cloud_parcel.monte_carlo import monte_carlo
+from pyrcel import AerosolSpecies, Lognorm
 
 __version__ = "0.1.0"
 __author__ = "Max Muter"
